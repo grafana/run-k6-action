40b7636487cc15c8424db3bf2fe6cbeb8d0dddbe
Deivid Fortuna <deividfortuna@gmail.com>
2025-03-11 18:29:34 +1300

    add test to for issue with environment variables

--- dev/env-var-test.js
+++ dev/env-var-test.js
@@ -6,6 +6,8 @@ export const options = {
   },
 }
 
+const data = JSON.parse(open(`./${__ENV.ENVIRONMENT}-data.json`))
+
 export default function () {
   check(__ENV, {
     'System env var': (env) =>
@@ -14,5 +16,9 @@ export default function () {
       env.TEST_FLAG_ENV_VAR === 'test-flag-env-var-value',
     'CLI -e flag set': (env) =>
       env.TEST_SYSTEM_ENV_VAR_E === 'test-cli-e-env-var-value',
+  })
+
+  check(data, {
+    'Username is john_doe': (data) => data.username == 'john_doe',
   })
 }
