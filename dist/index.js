e9b6cc951423a66662c53412c3c93d041b394150
ryan.pratt <ryan.pratt@justeattakeaway.com>
2025-04-15 11:28:38 -0500

    Update flag splitting with string-argv

--- dist/index.js
+++ dist/index.js
@@ -35565,6 +35565,7 @@ const analytics_1 = __nccwpck_require__(1267);
 const githubHelper_1 = __nccwpck_require__(2384);
 const k6helper_1 = __nccwpck_require__(5354);
 const utils_1 = __nccwpck_require__(1798);
+const string_argv_1 = __nccwpck_require__(1024);
 const TEST_PIDS = [];
 run();
 /**
@@ -35592,7 +35593,7 @@ async function run() {
         if (testPaths.length === 0) {
             throw new Error('No test files found');
         }
-        const verifiedTestPaths = await (0, k6helper_1.validateTestPaths)(testPaths, inspectFlags ? inspectFlags.split(' ') : []);
+        const verifiedTestPaths = await (0, k6helper_1.validateTestPaths)(testPaths, inspectFlags ? (0, string_argv_1.parseArgsStringToArgv)(inspectFlags) : []);
         if (verifiedTestPaths.length === 0) {
             throw new Error('No valid test files found');
         }
@@ -35958,6 +35959,7 @@ const child_process_1 = __nccwpck_require__(5317);
 const path_1 = __importDefault(__nccwpck_require__(6928));
 const apiUtils_1 = __nccwpck_require__(890);
 const k6OutputParser_1 = __nccwpck_require__(7076);
+const string_argv_1 = __nccwpck_require__(1024);
 function getK6CloudBaseUrl() {
     return process.env.K6_CLOUD_BASE_URL || 'https://api.k6.io';
 }
@@ -36077,7 +36079,7 @@ function generateK6RunCommand(path, flags, isCloud, cloudRunLocally) {
     return command;
 }
 function executeRunK6Command(command, totalTestRuns, testResultUrlsMap, debug) {
-    const parts = command.split(' ');
+    const parts = (0, string_argv_1.parseArgsStringToArgv)(command);
     const cmd = parts[0];
     const args = parts.slice(1);
     console.log(`🤖 Running test: ${cmd} ${args.join(' ')}`);
@@ -38449,6 +38451,59 @@ function parseParams (str) {
 }
 
 module.exports = parseParams
+
+
+/***/ }),
+
+/***/ 1024:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+exports.__esModule = true;
+exports.parseArgsStringToArgv = void 0;
+function parseArgsStringToArgv(value, env, file) {
+    // ([^\s'"]([^\s'"]*(['"])([^\3]*?)\3)+[^\s'"]*) Matches nested quotes until the first space outside of quotes
+    // [^\s'"]+ or Match if not a space ' or "
+    // (['"])([^\5]*?)\5 or Match "quoted text" without quotes
+    // `\3` and `\5` are a backreference to the quote style (' or ") captured
+    var myRegexp = /([^\s'"]([^\s'"]*(['"])([^\3]*?)\3)+[^\s'"]*)|[^\s'"]+|(['"])([^\5]*?)\5/gi;
+    var myString = value;
+    var myArray = [];
+    if (env) {
+        myArray.push(env);
+    }
+    if (file) {
+        myArray.push(file);
+    }
+    var match;
+    do {
+        // Each call to exec returns the next regex match as an array
+        match = myRegexp.exec(myString);
+        if (match !== null) {
+            // Index 1 in the array is the captured group if it exists
+            // Index 0 is the matched text, which we use if no captured group exists
+            myArray.push(firstString(match[1], match[6], match[0]));
+        }
+    } while (match !== null);
+    return myArray;
+}
+exports["default"] = parseArgsStringToArgv;
+exports.parseArgsStringToArgv = parseArgsStringToArgv;
+// Accepts any number of arguments, and returns the first one that is a string
+// (even an empty string)
+function firstString() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    for (var i = 0; i < args.length; i++) {
+        var arg = args[i];
+        if (typeof arg === "string") {
+            return arg;
+        }
+    }
+}
 
 
 /***/ })
